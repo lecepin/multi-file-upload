ca6d341e7f475eae1991f88956580e525f74a8e6
快乐平方 <383810086@qq.com>
2021-04-30 00:10:57 +0800

    feat: bat copy

--- start.bat
+++ start.bat
@@ -1,6 +1,11 @@
+:: 找本机IP
 @echo off
 for /f "tokens=4" %%a in ('route print^|findstr 0.0.0.0.*0.0.0.0') do (
  set IP=%%a
 )
 
+:: 将网址进行复制
+echo http://%IP%:8000 |clip 
+
+:: 启动服务
 php -c ./ -S %IP%:8000
