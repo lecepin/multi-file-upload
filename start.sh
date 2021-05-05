90f2a09506cfcc2bd8cc6586b09b320e0ee02ab3
快乐平方 <383810086@qq.com>
2021-05-05 23:17:26 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ip="$(ifconfig | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $2}' | tr -d "addr:")"
-echo http://$ip:8000 | pbcopy
+# echo http://$ip:8000 | pbcopy
 cd $(dirname $0)
+open http://$ip:8000
 php -c ./ -S $ip:8000
